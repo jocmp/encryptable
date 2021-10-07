4b58d6d612c649a3d197278ab43b5da2354a47c9
Josiah Campbell <jocmp64@gmail.com>
2021-10-07 10:25:19 -0400

    Update commentary in Encryptable module

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -15,16 +15,17 @@ module Encryptable
       # In ActiveRecord, this could be created using a ActiveRecord migration.
       encrypted_attr_name = "#{attr_name}_encrypted"
 
-      # Define the accessor (getter and setter) for the class.
-      # For example, this creates the CreditCard#number and CreditCard#number=()
-      # methods without depending on an additional declaration. This depends on some base
-      # class functionality, specifically modelled after ActiveRecord, that will already have
-      # defined the encrypted-suffixed fields at runtime.
+      # Define the getter for the subclass. The defined getter assumes
+      # that the underlying value can be decrypted by the algorithm in the
+      # private "decrypt" method.
       define_method(attr_name) do
         value = send(encrypted_attr_name)
         decrypt(value)
       end
 
+      # The defined setter always encrypts the value before assigning
+      # the encrypted field. In Rails specifically, assignment occurs
+      # before an ActiveRecord column update.
       define_method("#{attr_name}=") do |value|
         send("#{encrypted_attr_name}=", encrypt(value))
       end
@@ -33,9 +34,9 @@ module Encryptable
 
   private
 
-  # This is a Base16 conversion and not encryption whatsoever!
-  # In a real application this would be replaced with salt, a hashing algo,
-  # and other security measures
+  # This is a hexadecimal conversion and not encryption whatsoever!
+  # In a real application this would be replaced with salt,
+  # a hashing algorithm, and other security measures
   def encrypt(value)
     value.unpack("H*").first
   end
